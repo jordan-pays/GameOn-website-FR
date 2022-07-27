429fda8c36fe3f2fa772f4bdf60592cb49809cf8
Jordan-Pays-LMKSystem <jordan@lmksystem.fr>
2022-07-27 09:41:32 +0200

    fin projet

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,36 +1,34 @@
 function editNav() {
   var x = document.getElementById("myTopnav");
 
-  document.getElementById("mainNavbar").style.marginRight = "40px"
+  document.getElementById("mainNavbar").style.paddingRight = "40px"
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
-  document.getElementById("mainNavbar").style.marginRight = "0px"
+    document.getElementById("mainNavbar").style.paddingRight = "0px"
     x.className = "topnav";
   }
 }
 
-function dateMax (){
+function dateMax() {
   let today = new Date();
   let day = today.getDate();
-  let month = today.getMonth() +1;
+  let month = today.getMonth() + 1;
   let year = today.getFullYear() - 13;
-  
+
   if (day < 10) {
     day = '0' + day;
   }
-  
+
   if (month < 10) {
     month = '0' + month;
-  } 
-      
+  }
+
   let limit = year + '-' + month + '-' + day;
-  date_input.setAttribute("max", limit); 
+  date_input.setAttribute("max", limit);
 }
 
 const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-const carRegExp = /[a-zA-Z]+/g // ou /[^0-9]+/g
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
@@ -64,41 +62,28 @@ function closeModal() {
 }
 
 // Chek the first name
-function isValidFirst (){
-  debugger
-  if(first_input.value == "" || first_input.value.length<2 ){
+function isValidFirst() {
+  if (first_input.value == "" || first_input.value.length < 2) {
     first_input.parentElement.setAttribute('data-error-visible', 'true');
     first_input.parentElement.setAttribute('data-error', 'Votre prénom doit contenir au moins 2 lettres');
     return false
-  }else{
-    if(carRegExp.test(first_input.value)){
-      first_input.parentElement.setAttribute('data-error-visible', 'false');
-      return true
-    }else{
-      first_input.parentElement.setAttribute('data-error-visible', 'true');
-      first_input.parentElement.setAttribute('data-error', 'Votre prénom ne peut pas contenir de chiffre');
-      return false
-    }
-    
+  } else {
+    first_input.parentElement.setAttribute('data-error-visible', 'false');
+    return true
+
   }
 }
 
 // Chek the last name
-function isValidLast () {
-  if(last_input.value == "" || last_input.value.length<2 ){
+function isValidLast() {
+  if (last_input.value == "" || last_input.value.length < 2) {
     last_input.parentElement.setAttribute('data-error-visible', 'true');
     last_input.parentElement.setAttribute('data-error', 'Votre nom doit contenir au moins 2 lettres');
     return false
-  }else{
-    if(carRegExp.test(last_input.value)){
-      last_input.parentElement.setAttribute('data-error-visible', 'false');
-      return true
-    }else{
-      last_input.parentElement.setAttribute('data-error-visible', 'true');
-      last_input.parentElement.setAttribute('data-error', 'Votre nom ne peut pas contenir de chiffre');
-      return false
-    }
-    
+  } else {
+    last_input.parentElement.setAttribute('data-error-visible', 'false');
+    return true
+
   }
 }
 
@@ -116,11 +101,11 @@ function isValidEmail() {
 
 //Check if date is entered
 function isValidDate() {
-  if(date_input.value==""){
+  if (date_input.value == "") {
     date_input.parentElement.setAttribute('data-error-visible', 'true');
     date_input.parentElement.setAttribute('data-error', 'Veuillez renseigner une date de naissance');
     return false
-  }else{
+  } else {
     date_input.parentElement.setAttribute('data-error-visible', 'false');
     return true
   }
@@ -128,11 +113,11 @@ function isValidDate() {
 
 //Check if quantity   is entered
 function isValidQuantity() {
-  if(quantity_input.value==""){
+  if (quantity_input.value == "") {
     quantity_input.parentElement.setAttribute('data-error-visible', 'true');
     quantity_input.parentElement.setAttribute('data-error', 'Veuillez renseigner une valeur');
     return false
-  }else{
+  } else {
     quantity_input.parentElement.setAttribute('data-error-visible', 'false');
     return true
   }
@@ -156,10 +141,10 @@ function isValidLocation() {
 
 //Check if the cu has been checked
 function isValidCu() {
-  if(cu_check.checked){
+  if (cu_check.checked) {
     cu_check.parentElement.setAttribute('data-error-visible', 'false');
     return true
-  }else{
+  } else {
     cu_check.parentElement.setAttribute('data-error-visible', 'true');
     cu_check.parentElement.setAttribute('data-error', 'Vous êtes obliger de valider les conditions d\'utilisations');
     return false
@@ -167,24 +152,56 @@ function isValidCu() {
 }
 
 
-first_input.addEventListener("change",isValidFirst)
-last_input.addEventListener("change",isValidLast)
-email_input.addEventListener("change",isValidEmail)
-date_input.addEventListener("change",isValidDate)
-quantity_input.addEventListener("change",isValidQuantity)
-cu_check.addEventListener("change",isValidCu)
+first_input.addEventListener("change", isValidFirst)
+last_input.addEventListener("change", isValidLast)
+email_input.addEventListener("change", isValidEmail)
+date_input.addEventListener("change", isValidDate)
+quantity_input.addEventListener("change", isValidQuantity)
+cu_check.addEventListener("change", isValidCu)
 
 function validate() {
-  if(isValidFirst() && isValidLast() && isValidEmail() && isValidDate() && isValidQuantity() && isValidLocation() && isValidCu()){
+  if (isValidFirst() && isValidLast() && isValidEmail() && isValidDate() && isValidQuantity() && isValidLocation() && isValidCu()) {
     formData.forEach(element => {
       element.style.display = "none"
     });
     document.getElementById("valid_div").setAttribute("style", "height:412px;align-items:center;justify-content:center;display:flex")
     document.getElementById("valid_div").innerHTML = '<h1 class="valid_text"> Merci pour votre inscription </h1>'
     document.getElementById("submit").value = "Femer";
-    document.getElementById("submit").addEventListener("click",closeModal)
+    document.getElementById("submit").addEventListener("click", closeModal)
     return false
-  }else{
+  } else {
     return false
   }
+}
+
+
+/*Partie navigation */
+
+function clickNav(id) {
+  const tabNav = document.getElementsByClassName("element_nav")
+  let i = 0
+  while (i < tabNav.length) {
+    const isActiveBis = isActive("active", tabNav[i])
+    if (isActiveBis && i != id) {
+      removeActive("active", tabNav[i])
+    } else if (!isActiveBis && i == id) {
+      addActive("active", tabNav[i])
+    }
+
+    i++
+  }
+
+  addActive("active", tabNav[id])
+}
+
+function isActive(classActive, dom) {
+  return dom.classList.contains(classActive)
+}
+
+function removeActive(classActive, dom) {
+  dom.classList.remove(classActive)
+}
+
+function addActive(classActive, dom) {
+  dom.classList.add(classActive)
 }
